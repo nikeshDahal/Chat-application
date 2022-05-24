f2e791893ba88a403f930d015425d2ed68d7d609
nikeshDahal <nikeshdahal297@gmail.com>
2022-05-24 13:24:11 +0545

    file attachments rendered on chat-box

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const mainListForNewGroups = document.getElementById("new-group");
 const mainListForGroupMembers = document.getElementById("group-members-id");
 const createdUsers = document.getElementById("users");
 const generateUserList = document.getElementById("user-List");
+const inputOfImage=document.getElementById('image_input');
+
 console.log(inputField, sendImageButton);
+
 const newGroups = [];
-//for creation of user
 const newUser = [];
 const dynamicElement = [];
 let inputValueOfNewUser;
-const buttonElement = document.getElementById("create-user-button");
+
+//for creation of user
 function creationNewUserHandler() {
   const createNewUserElement = document.getElementById("create-user-id");
   inputValueOfNewUser = createNewUserElement.value;
@@ -45,9 +48,10 @@ function creationNewUserHandler() {
     eventListener(item);
   }
 }
+const buttonElement = document.getElementById("create-user-button");
 buttonElement.addEventListener("click", creationNewUserHandler);
-
 //new user creation completed
+
 let selectedUser;
 function eventListener(element) {
   element.addEventListener("click", () => {
@@ -66,27 +70,44 @@ function validate(value, flag) {
 //for validation completed
 
 //send image button is clicked and mesages are rendered in page by creating elements.
+let newMessageElement
+
+
 function sendMessageFunction() {
-  if (!validate(`${inputField.value}`, REQUIRED)) {
-    alert("please type some message in message box below");
+  let inputFieldValue=inputField.value
+  
+  if (!selectedUser) {
+    alert(
+      "please select any user from the sidebar to send message from their name in group,so that other user may know the sender"
+    );
+    inputField.value = "";
   } else {
-    if (!selectedUser) {
-      alert(
-        "please select any user from the sidebar to send message from their name in group,so that other user may know the sender"
-      );
-      inputField.value = "";
-    }
-      const newMessageElement = document.createElement("li");
-      newMessageElement.innerHTML = `${selectedUser}: ${inputField.value.replace(
-        /<[^>]+>/g,
-        ""
-      )}`;
+
+    let fileNames = document.querySelector('#image_input');
+    let fileName = fileNames.value;
+    extension = fileName.substring(fileName.lastIndexOf('.') + 1);
+    if (extension=='png'|| extension=='jpg' ||extension=='jpeg'){
+      let file=window.URL.createObjectURL(fileNames.files[0]);
+      newMessageElement = document.createElement("li");
+      newMessageElement.innerHTML = `${selectedUser}:<img src="${file}" alt="" width="180 " height="100">`;
       newMessageElement.className = "message-item item-primary";
-      // newMessageElement.style.justifyContent="between";
+      mainMessageList.appendChild(newMessageElement); 
+    }
+    else if(!validate(`${inputFieldValue}`, REQUIRED)) {
+      alert("please type some message in message box below");
+    }else{
+      newMessageElement = document.createElement("li");
+      newMessageElement.innerHTML = `${selectedUser}: ${inputFieldValue.replace(
+          /<[^>]+>/g,
+          ""
+        )}`;
+      newMessageElement.className = "message-item item-primary";
       newMessageElement.style.overflow = "break-word";
       mainMessageList.appendChild(newMessageElement);
       inputField.value = "";
     }
+
+  }
 }
 sendImageButton.addEventListener("click", () => {
   sendMessageFunction();
@@ -94,21 +115,20 @@ sendImageButton.addEventListener("click", () => {
 //send button ended
 
 //group-portion-started
+//generation of listed  users in option block
+const groupNameElement = document.getElementById("group-name");
 function generateUserLIstHandler() {
   newUser.forEach((userOption) => {
     console.log("arrayofusers", userOption);
     const options = document.createElement("option");
     options.innerHTML = `
     <option value="${userOption}Id">${userOption}</option>
-
     `;
     createdUsers.appendChild(options);
-    // "usersForGroup('${inputValueOfNewGroup}')"
   });
 }
 let inputValueOfNewGroup;
 let enteredGroupName;
-const groupNameElement = document.getElementById("group-name");
 groupNameElement.addEventListener("keypress", function (event) {
   enteredGroupName = groupNameElement.value;
   if (event.key === "Enter") {
@@ -119,6 +139,8 @@ groupNameElement.addEventListener("keypress", function (event) {
 
   mainListForGroupMembers.innerHTML = ``;
 });
+
+
 function selectionOfUsersToAddInGroup() {
   let usersSelected = document.getElementById("users").value;
   console.log(newGroups, "testingphase");
@@ -136,8 +158,10 @@ function selectionOfUsersToAddInGroup() {
   });
 }
 generateUserList.addEventListener("click", generateUserLIstHandler);
+//user list generation ended
+
 //for creation of Group
-const buttonElementGroup = document.getElementById("create-group-button");
+
 function creationNewGroupHandler() {
   inputValueOfNewGroup = groupNameElement.value;
   console.log(inputValueOfNewGroup);
@@ -164,13 +188,13 @@ function creationNewGroupHandler() {
   mainListForNewGroups.appendChild(newGroupElement);
   groupNameElement.value = "";
 }
+const buttonElementGroup = document.getElementById("create-group-button");
 buttonElementGroup.addEventListener("click", creationNewGroupHandler);
 
 function eventListenerForGroupMembers(groupElement) {
   document.getElementById(
     "mainHeader"
   ).innerText = `${groupElement} Group-Chat`;
-
   mainListForGroupMembers.innerHTML = ``;
   console.log("item", groupElement);
   newGroups.forEach((singleGroup) => {
